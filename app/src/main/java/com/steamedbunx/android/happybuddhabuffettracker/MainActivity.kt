7a566d68a274ccbd933043d111699f04ca4e0c9b
Yi Hao <yi.hao01@outlook.com>
2019-09-27 14:32:32 -0500

    Initial commit

---
+++ app/src/main/java/com/steamedbunx/android/happybuddhabuffettracker/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.steamedbunx.android.happybuddhabuffettracker
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
